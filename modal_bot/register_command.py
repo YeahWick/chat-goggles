dd5e61ac632876be6fee24740aa039b1fc53c917
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-05-28 03:01:20 +0000

    register command

--- modal_bot/register_command.py
+++ modal_bot/register_command.py
@@ -1,21 +1,31 @@
-import requests
+from modal import Stub, Secret
 
-url = "https://discord.com/api/v10/applications/<my_application_id>/guilds/<guild_id>/commands"
+stub = Stub()
 
-# This is an example USER command, with a type of 2
-json = {
-    "name": "High Five",
-    "type": 2
-}
-
-# For authorization, you can use either your bot token
-headers = {
-    "Authorization": "Bot <my_bot_token>"
-}
-
-# or a client credentials token for your app with the applications.commands.update scope
-headers = {
-    "Authorization": "Bearer <my_credentials_token>"
-}
-
-r = requests.post(url, headers=headers, json=json)
+@stub.function(secret=[Secret.from_name("discord-bot")])
+async def reg_command():
+    import requests
+    
+    guild_id = os.getenv("GUILD_ID")
+    app_id = os.getenv("APP_ID")
+    bot_token = os.getenv("BOT_TOKEN")
+    
+    url = f"https://discord.com/api/v10/applications/{app_id}/guilds/{guild_id}/commands"
+    
+    # This is an example USER command, with a type of 2
+    json = {
+        "name": "High Five",
+        "type": 2
+    }
+    
+    # For authorization, you can use either your bot token
+    headers = {
+        "Authorization": f"Bot {bot_token}"
+    }
+    
+    # or a client credentials token for your app with the applications.commands.update scope
+    #headers = {
+    #    "Authorization": "Bearer <my_credentials_token>"
+    #}
+    
+    requests.post(url, headers=headers, json=json, timeout=15)
