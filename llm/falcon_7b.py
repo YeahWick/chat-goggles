289b00c81b4f58a50e0244e0ae9b94d2570023b2
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-05-30 13:15:52 +0000

    .

--- llm/falcon_7b.py
+++ llm/falcon_7b.py
@@ -5,8 +5,37 @@ def download_model():
 
     snapshot_download(repo_id="tiiuae/falcon-7b-instruct", revision="9cbb1610be271fd06576e7e00f60e75612d53a4d")
 
+#This doesn't work. Maybe put something similar into a modal __init__
+def warm_model():
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    import transformers
+    import torch
+    
+    model = "tiiuae/falcon-7b-instruct"
+    
+    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True, revision="9cbb1610be271fd06576e7e00f60e75612d53a4d")
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+    sequences = pipeline(
+       "What is the air speed velocity of a fully laden swallow?",
+        max_length=400,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    for seq in sequences:
+        print(f"Result: {seq['generated_text']}")
+
 stub = Stub()
-image = Image.debian_slim().pip_install( "transformers", "torch", "einops", "huggingface_hub").run_function(download_model).pip_install("accelerate")
+image = Image.debian_slim().pip_install("transformers", "torch", "einops",
+                                        "huggingface_hub").run_function(download_model).pip_install("accelerate").run_function(warm_model)
 
 
 @stub.function(image=image, gpu="any")
@@ -27,7 +56,7 @@ async def llm_command():
         device_map="auto",
     )
     sequences = pipeline(
-       "What is the air speed velocity of a fully laden swallow?",
+       "Please write out some code to post images with an interactions discord bot in python",
         max_length=400,
         do_sample=True,
         top_k=10,
