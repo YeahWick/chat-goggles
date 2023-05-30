8b3fcee6c8d07089fcb1b7adcc2509e413bf61fc
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-05-30 20:59:17 +1000

    Modal bot start (#2)
    discord bot start

---
+++ modal_bot/modal_bot.py
@@ -0,0 +1,60 @@
+from modal import Image, Stub, wsgi_app, Secret, is_local, Dict
+from os import getenv
+
+stub = Stub("bot-v2")
+image = Image.debian_slim().pip_install( "flask", "discord_interactions")
+
+# Set environment
+if is_local():
+    env = getenv("DISCORD_BOT_ENV")
+    stub.data_dict = Dict({"env": env})
+
+@stub.function(image=image,
+               secret=Secret.from_name("discord-bot"))
+@wsgi_app()
+def flask_app():
+    from flask import Flask, request, jsonify
+    from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
+    from os import getenv
+    from time import strftime
+    import json, sys
+
+    web_app = Flask(__name__)
+    environment = stub.app.data_dict["env"]
+    print(f"env is: {environment}")
+
+    app_config = dict(
+        dev=dict(
+            CLIENT_PUBLIC_KEY=getenv('CLIENT_PUBLIC_KEY')
+        ),
+        prod=dict(
+            CLIENT_PUBLIC_KEY=getenv('CLIENT_PUBLIC_KEY_PROD')
+        )
+    )
+
+    CLIENT_PUBLIC_KEY = app_config.get(environment).get('CLIENT_PUBLIC_KEY')
+
+    @web_app.get("/")
+    def home():
+        return "Hello Flask World!"
+
+    @web_app.post('/interactions')
+    @verify_key_decorator(CLIENT_PUBLIC_KEY)
+    def interactions():
+        if request.json['type'] == InteractionType.APPLICATION_COMMAND:
+            msgs = " ".join([v['content'] for k,v in request.json['data']['resolved']['messages'].items()])
+            return jsonify({
+                'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                'data': {
+                    'content': f"responding to: {msgs}"
+                }
+            })
+            #call some other modal function to return render
+
+    @web_app.after_request
+    def after_request(response):
+        timestamp = strftime('[%Y-%b-%d %H:%M]')
+        print(f"{timestamp}, remote add: {request.remote_addr}, method: {request.method}, scheme: {request.scheme}, path: {request.full_path}, status: {response.status}")
+        return response
+
+    return web_app
