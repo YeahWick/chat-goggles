1568599f281cd211dbbe4d2b6a9c43654798a1bc
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-06-01 03:41:30 +0000

    pretrain in image. manual eos token

--- llm/falcon_7b.py
+++ llm/falcon_7b.py
@@ -5,40 +5,17 @@ def download_model():
 
     snapshot_download(repo_id="tiiuae/falcon-7b-instruct", revision="9cbb1610be271fd06576e7e00f60e75612d53a4d")
 
-#This doesn't work. Maybe put something similar into a modal __init__
-def warm_model():
-    from transformers import AutoTokenizer, AutoModelForCausalLM
-    import transformers
-    import torch
-    
+def get_pretrained():
+    from transformers import AutoTokenizer
     model = "tiiuae/falcon-7b-instruct"
-    
-    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True, revision="9cbb1610be271fd06576e7e00f60e75612d53a4d")
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        device_map="auto",
-    )
-    sequences = pipeline(
-       "What is the air speed velocity of a fully laden swallow?",
-        max_length=400,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-    )
-    for seq in sequences:
-        print(f"Result: {seq['generated_text']}")
+    AutoTokenizer.from_pretrained(model, trust_remote_code=True, revision="9cbb1610be271fd06576e7e00f60e75612d53a4d")
 
 stub = Stub()
 image = Image.debian_slim().pip_install("transformers", "torch", "einops",
-                                        "huggingface_hub").run_function(download_model).pip_install("accelerate").run_function(warm_model)
+                                        "huggingface_hub").run_function(download_model).pip_install("accelerate").run_function(get_pretrained)
 
 
-@stub.function(image=image, gpu="any")
+@stub.function(image=image, gpu="A100")
 async def llm_command():
     from transformers import AutoTokenizer, AutoModelForCausalLM
     import transformers
@@ -61,7 +38,8 @@ async def llm_command():
         do_sample=True,
         top_k=10,
         num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
+        #eos_token_id=tokenizer.eos_token_id,
+        eos_token_id=11, #bug see https://huggingface.co/TheBloke/falcon-40b-instruct-GPTQ/discussions/8
     )
     for seq in sequences:
         print(f"Result: {seq['generated_text']}")
