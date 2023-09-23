c652c627bd2ea5856fc7408c899862f1f47aa46a
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-09-23 22:42:11 +0000

    xwin 70b test

--- llm/xwin_lm_70b.py
+++ llm/xwin_lm_70b.py
@@ -9,11 +9,11 @@ def download_model():
     # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
     llm = AutoModelForCausalLM.from_pretrained(model_name, model_file=model_file, model_type="llama")# , gpu_layers=50)
 
-stub = Stub('xwin_lm')
+stub = Stub('xwin_lm_70b')
 image = Image.debian_slim().pip_install("ctransformers").run_function(download_model)
 
 @stub.function(image=image)
 def entry_function():
     from ctransformers import AutoModelForCausalLM
     llm = AutoModelForCausalLM.from_pretrained(model_name, model_file=model_file, model_type="llama", local_files_only=True)# , gpu_layers=50)
-    print(llm("Here is a bash function that will change a aws ec2 tag", max_new_tokens=400))
+    print(llm("What is the carry capacity of a full laden swallow?", max_new_tokens=400))
