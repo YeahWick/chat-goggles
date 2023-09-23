fb1c4b8caf7ae6b3c6adec148cd24f40ca692a26
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-09-23 11:45:39 +0000

    testing more models

---
+++ llm/xwin_lm.py
@@ -0,0 +1,16 @@
+from modal import Stub, Image, Function
+
+def download_model():
+    from ctransformers import AutoModelForCausalLM
+
+    # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Xwin-LM-13B-V0.1-GGUF", model_file="xwin-lm-13b-v0.1.Q4_K_M.gguf", model_type="llama")# , gpu_layers=50)
+
+stub = Stub('robin')
+image = Image.debian_slim().pip_install("ctransformers").run_function(download_model)
+
+@stub.function(image=image)
+def entry_function():
+    from ctransformers import AutoModelForCausalLM
+    llm = AutoModelForCausalLM.from_pretrained("TheBloke/Xwin-LM-13B-V0.1-GGUF", model_file="xwin-lm-13b-v0.1.Q4_K_M.gguf", model_type="llama", local_files_only=True)# , gpu_layers=50)
+    print(llm("Write bash function to change a aws ec2 tag"))
