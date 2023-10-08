e5a85b34d558e076208cb8a7f9c2a30222989d1f
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-10-08 10:45:54 +0000

    more

--- run_model/code_sandbox.py
+++ run_model/code_sandbox.py
@@ -1,17 +1,27 @@
 from modal import Stub, Mount, Image, Function
+import hashlib
 
 stub = Stub("code-sandbox")
 image = Image.debian_slim()
 
 @stub.function(image=image)
 def code_callback(code: str):
-    with open("code.py", 'w') as f:
+    code_sha = hashlib.sha1(code.encode()).hexdigest()
+    filename = f"{code_sha}.py"
+
+    print("\n\nhere\n")
+    print(code)
+    print("\n\nhere3\n")
+    print(code)
+    print("\nhere4\n")
+
+    with open(filename, 'w') as f:
         f.write(code)
 
     sb = stub.spawn_sandbox(
-        "python", "/repo/code.py",
+        "python", f"/repo/{filename}",
         image=image,
-        mounts=[Mount.from_local_file(local_path="./code.py", remote_path="/repo/code.py")],
+        mounts=[Mount.from_local_file(local_path=f"./{filename}", remote_path=f"/repo/{filename}")],
         timeout=60,
     )
     sb.wait()
