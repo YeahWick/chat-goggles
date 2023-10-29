c6839843a12954205318775f3cd27f6a32c3b500
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-10-29 11:04:53 +0000

    dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,16 +1,25 @@
-from datasets import load_dataset
+from modal import Stub, Image, Volume, Function, Queue, method
 
-# https://huggingface.co/datasets/tasksource/bigbench
-# abstract_narrative_understanding
-def fetch_huggingface_data(dataset):
-    huggingface_dataset = load_dataset(dataset)
+stub = Stub('datasets')
+stub.volume = Volume.persisted("datasets")
+
+vol_mnt = "/datasets"
+
+image = Image.debian_slim().pip_install(["datasets"])
+
+@stub.function(image=image, volumes={vol_mnt: stub.volume})
+def fetch_data(dataset):
+    from datasets import load_dataset
+    #TODO mkdir
+    huggingface_dataset = load_dataset(dataset_repo, dataset_name, cache_dir = f"{vol_mnt}/{dataset_repo}")
     def data_generator():
         for example in huggingface_dataset['train']:
             yield example
     return data_generator
 
-# Running the function for "squad" dataset
-gen = fetch_huggingface_data('squad')
-for item in gen():
-    print(item)
-    break
+@stub.local_entrypoint()
+def main():
+    gen = fetch_data("tasksource/bigbench", "abstract_narrative_understanding")
+    for item in gen():
+        print(item)
+        break
