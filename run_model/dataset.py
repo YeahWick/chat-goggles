19a825cb473521bfd6631b39ab3dc6e57d154eec
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-10-31 12:13:58 +0000

    dataset get fix

--- run_model/dataset.py
+++ run_model/dataset.py
@@ -9,25 +9,39 @@ vol_mnt = "/datasets"
 
 image = Image.debian_slim().pip_install(["datasets"])
 
-@stub.function(image=image, volumes={vol_mnt: stub.volume})
-def fetch_data(dataset_repo, dataset_name):
-    from datasets import load_dataset
-    huggingface_dataset = load_dataset(dataset_repo, dataset_name, cache_dir = f"{vol_mnt}/")
-    def data_generator():
-        for example in huggingface_dataset['train']:
-            yield example
-    return data_generator
+class DataIterator(object):
+    from datasets import Dataset
+
+    def __init__(self, rdataset: Dataset):
+        self.rdataset = rdataset
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.rdataset):
+            raise StopIteration
+        else:
+            data = self.rdataset[self.index]
+            self.index += 1
+            return data
+
+def fetch_data(dataset_repo, dataset_name, split):
+    from datasets import load_dataset, DownloadConfig
+    stub.volume.reload()
+    load_dataset(dataset_repo, dataset_name, cache_dir = f"{vol_mnt}/", download_config=DownloadConfig(cache_dir=f"{vol_mnt}/downloads/"))
+    stub.volume.commit()
+    huggingface_dataset = load_dataset(dataset_repo, dataset_name, cache_dir = f"{vol_mnt}/", download_config=DownloadConfig(cache_dir=f"{vol_mnt}/downloads/"))
+    return DataIterator(huggingface_dataset[split])
 
 @stub.function(image=image, volumes={vol_mnt: stub.volume})
-def put_queue(data_gen): #TODO add type
-    #TODO fix error
-    # No module named 'datasets'
-    # This can happen if your local environment does not have a module that was used to construct the result. 
-    for item in data_gen():
+def put_queue(dataset_repo, dataset_name):
+    data_it = fetch_data(dataset_repo, dataset_name,"train")
+    for item in data_it:
         print(item)
         break
 
 @stub.local_entrypoint()
 def main():
-    gen = fetch_data.remote("tasksource/bigbench", "abstract_narrative_understanding")
-    put_queue.remote(gen)
+    put_queue.remote("tasksource/bigbench", "abstract_narrative_understanding")
