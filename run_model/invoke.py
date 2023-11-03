ff1ad4349a06e95a5e97e7e6eebbf4595fed5d67
YeahWick <133565833+YeahWick@users.noreply.github.com>
2023-11-03 02:36:37 +0000

    run with ctranslate2

--- run_model/invoke.py
+++ run_model/invoke.py
@@ -7,36 +7,130 @@ stub.queue = Queue.persisted("invoke-queue")
 
 vol_mnt = "/models"
 
-image = Image.debian_slim().env(dict(CMAKE_ARGS="-DLLAMA_CUBLAS=on")).pip_install(["huggingface_hub", "llama-cpp-python"])
+image = Image.debian_slim().pip_install(["huggingface_hub", "ctranslate2", "torch", "transformers"])
+
+
 
 @stub.function(image=image, volumes={vol_mnt: stub.volume})
 def download(args: DownloadArgs):
     from huggingface_hub import hf_hub_download
+    from ctranslate2.converters import TransformersConverter
+    from pathlib import Path
     import os, shutil
 
     target_dir = f"{vol_mnt}/{args.repo_name}"
     os.makedirs(target_dir, exist_ok=True)
 
-    downloads_dir = "/model_downloads"
-    os.mkdir(downloads_dir)
-    model_file = hf_hub_download(repo_id=args.repo_name, filename=args.file_name, cache_dir=downloads_dir)
+    #TODO move this
+    import torch
+    import gc
+    from ctranslate2.converters.transformers import register_loader, ModelLoader
+    from ctranslate2.specs import (
+        common_spec,
+        transformer_spec,
+    )
+    @register_loader("MistralConfig")
+    class MistralLoader(ModelLoader):
+        @property
+        def architecture_name(self):
+            return "MistralForCausalLM"
 
-    shutil.copy(model_file, target_dir)
+        def get_model_spec(self, model):
+            num_layers = model.config.num_hidden_layers
+
+            num_heads = model.config.num_attention_heads
+            num_heads_kv = getattr(model.config, "num_key_value_heads", num_heads)
+            if num_heads_kv == num_heads:
+                num_heads_kv = None
+
+            spec = transformer_spec.TransformerDecoderModelSpec.from_config(
+                num_layers,
+                num_heads,
+                activation=common_spec.Activation.SWISH,
+                pre_norm=True,
+                ffn_glu=True,
+                rms_norm=True,
+                rotary_dim=0,
+                rotary_interleave=False,
+                num_heads_kv=num_heads_kv,
+            )
+
+            self.set_decoder(spec.decoder, model.model)
+            self.set_linear(spec.decoder.projection, model.lm_head)
+            return spec
+
+        def get_vocabulary(self, model, tokenizer):
+            tokens = super().get_vocabulary(model, tokenizer)
+
+            extra_ids = model.config.vocab_size - len(tokens)
+            for i in range(extra_ids):
+                tokens.append("<extra_id_%d>" % i)
+
+            return tokens
+
+        def set_vocabulary(self, spec, tokens):
+            spec.register_vocabulary(tokens)
+
+        def set_config(self, config, model, tokenizer):
+            config.bos_token = tokenizer.bos_token
+            config.eos_token = tokenizer.eos_token
+            config.unk_token = tokenizer.unk_token
+            config.layer_norm_epsilon = model.config.rms_norm_eps
+
+        def set_layer_norm(self, spec, layer_norm):
+            spec.gamma = layer_norm.weight
+
+        def set_decoder(self, spec, module):
+            spec.scale_embeddings = False
+            self.set_embeddings(spec.embeddings, module.embed_tokens)
+            self.set_layer_norm(spec.layer_norm, module.norm)
+
+            for layer_spec, layer in zip(spec.layer, module.layers):
+                self.set_layer_norm(
+                    layer_spec.self_attention.layer_norm, layer.input_layernorm
+                )
+                self.set_layer_norm(
+                    layer_spec.ffn.layer_norm, layer.post_attention_layernorm
+                )
+
+                wq = layer.self_attn.q_proj.weight
+                wk = layer.self_attn.k_proj.weight
+                wv = layer.self_attn.v_proj.weight
+                wo = layer.self_attn.o_proj.weight
+
+                layer_spec.self_attention.linear[0].weight = torch.cat([wq, wk, wv])
+                layer_spec.self_attention.linear[1].weight = wo
+
+                self.set_linear(layer_spec.ffn.linear_0, layer.mlp.gate_proj)
+                self.set_linear(layer_spec.ffn.linear_0_noact, layer.mlp.up_proj)
+                self.set_linear(layer_spec.ffn.linear_1, layer.mlp.down_proj)
+
+                delattr(layer, "self_attn")
+                delattr(layer, "mlp")
+                gc.collect()
+
+    converter = TransformersConverter(args.repo_name, low_cpu_mem_usage=True)
+    converter.convert(Path(f"{target_dir}/converted").as_posix())
     stub.volume.commit()
 
 @stub.function(image=image, volumes={vol_mnt: stub.volume}, timeout=1800)
 def invoke(args: InvokeArgs, callback: Function):
-    from llama_cpp import Llama
+    from ctranslate2 import Generator
+    from transformers import AutoTokenizer
     import os
 
     stub.volume.reload()
-    file_path = f"{vol_mnt}/{args.repo_name}/{args.file_name}"
-    if not os.path.isfile(file_path):
+    file_path = f"{vol_mnt}/{args.repo_name}"
+    if not os.path.isdir(f"{file_path}/converted"):
         download.remote(DownloadArgs(repo_name=args.repo_name, file_name=args.file_name))
         stub.volume.reload()
 
-    llm = Llama(f"{vol_mnt}/{args.repo_name}/{args.file_name}")
-    output = llm(args.prompt, max_tokens=args.context_length)
+    translator = Generator(f"{vol_mnt}/{args.repo_name}/converted")
+    tokenizer = AutoTokenizer.from_pretrained(args.repo_name) #TODO cache this too
+    prompt_tokenized = tokenizer.convert_ids_to_tokens(
+            tokenizer.encode(args.prompt)
+        )
+    output = translator.generate_batch([prompt_tokenized], max_length=args.context_length)
     if callback is None:
         print(output)
     else:
